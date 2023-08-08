3a1c3c1cb7d75054ae534fc8d26450ede126b124
Kazackoffyury <kazachkoff.yury@yandex.ru>
2023-08-09 00:07:52 +0300

    добавил хрень

--- src/main/java/com/yaga/model/Main.java
+++ src/main/java/com/yaga/model/Main.java
@@ -3,5 +3,6 @@ package com.yaga.model;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Юра,Привет");
+        System.out.println("хрень собачья");
     }
 }
