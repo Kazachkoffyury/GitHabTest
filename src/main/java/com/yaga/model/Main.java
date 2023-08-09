c817630df9ad2df1a79e793204d1b0f4d78cce6f
SergeiPetrov1974 <58001369+SergeiPetrov1974@users.noreply.github.com>
2023-08-09 17:57:32 +0300

    Принял

--- src/main/java/com/yaga/model/Main.java
+++ src/main/java/com/yaga/model/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Юра,Привет");
         System.out.println("хрень собачья");
+        System.out.println("Добавить, жеванного крота");
     }
 }
