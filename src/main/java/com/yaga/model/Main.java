379c8c9d25d6e9f25dfcf6cdf83327ec15aaef23
SergeiPetrov1974 <58001369+sergeipetrov1974@users.noreply.github.com>
2023-08-09 21:35:30 +0300

    Добавил цикл и вывод на консоль
    # Conflicts:
    #	src/main/java/com/yaga/model/Main.java

--- src/main/java/com/yaga/model/Main.java
+++ src/main/java/com/yaga/model/Main.java
@@ -2,7 +2,18 @@ package com.yaga.model;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Юра,Привет");
+        System.out.println("Юра,Привет!");
         System.out.println("хрень собачья");
+        System.out.println("Добавить, жеванного крота");
+        System.out.println("Йогурт, просроченный");
+        System.out.println("Бешенные кабаны бегут от денег из России");
+        System.out.println("Йогурт, не просроченный, а херовый");
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println(i + " Ты проебал День ");
+        }
+
+        System.out.println("тестируем черри пик/изначально закоммитил в мастер!!");
     }
 }
